849dcf94639d1d8693a2645153e33737e335d131
Administrator <Administrator@USER-20150927YN>
2015-10-08 10:13:08 +0800

    加注释

--- njby/src/com/njby/service/FileService.java
+++ njby/src/com/njby/service/FileService.java
@@ -8,8 +8,28 @@ import com.system.FileInfo;
 
 public interface FileService {
 	
+	/***
+	 * 上次文件有效性检查
+	 * 
+	 * @param fileType  文件类型
+	 * 
+	 * @param multipartFile  上传的文件
+	 * 
+	 * @return 是否有效
+	 */
 	public boolean isValid(FileInfo.FileType fileType, MultipartFile multipartFile);
 	
+	/**
+	 * 上传文件
+	 * 
+	 * @param request
+	 * 
+	 * @param fileType
+	 * 
+	 * @param multipartFile
+	 * 
+	 * @return 
+	 */
 	public String upload(HttpServletRequest request, FileInfo.FileType fileType, MultipartFile multipartFile);
 	
 }
